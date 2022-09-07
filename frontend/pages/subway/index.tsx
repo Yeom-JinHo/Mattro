932fcdc03cc27b496cf7835ebf7283c9e4667333
Yeom-JinHo <pwlsghq@naver.com>
2022-09-07 11:23:11 +0900

    feat : 노선도 초기셋

---
+++ frontend/pages/subway/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import MetroMap from "../../components/subway/MetroMap";
+
+const index = () => {
+  return (
+    <div>
+      <div>여기 헤더</div>
+      <MetroMap />
+    </div>
+  );
+};
+
+export default index;
