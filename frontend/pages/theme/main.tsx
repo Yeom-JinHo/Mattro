f260068d73bf6ef8e36c8a614101dd43ac6c87e2
jmlee9707 <jmlee9707@naver.com>
2022-09-08 11:28:26 +0900

    fix : PR code review 수정

--- frontend/pages/theme/main.tsx
+++ frontend/pages/theme/main.tsx
@@ -8,9 +8,9 @@ export default function themeMain() {
       <div className={`${styles.contents} flex column`}>
         <nav>
           <Link href="/">
-            <div className={`${styles.btn} fs-32 coreExtra`}>
+            <a className={`${styles.btn} fs-32 coreExtra`}>
               테마별 맛집 추천받기
-            </div>
+            </a>
           </Link>
         </nav>
       </div>
