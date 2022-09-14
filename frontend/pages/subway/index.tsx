ba755a8f7499b59fae716e50693f3d8234149000
Yeom-JinHo <pwlsghq@naver.com>
2022-09-14 15:47:52 +0900

    feat : 확대/축소 버튼

--- frontend/pages/subway/index.tsx
+++ frontend/pages/subway/index.tsx
@@ -1,9 +1,12 @@
+import Image from "next/image";
 import React from "react";
 import LineInfoList from "../../components/subway/LineCircleList";
 import LineSearch from "../../components/subway/LineSearch";
 import LineSelectedBar from "../../components/subway/LineSelectedBar";
 import MetroMap from "../../components/subway/MetroMap";
 import styles from "./subway.module.scss";
+import PlusBtn from "../../public/icons/plus.svg";
+import MinusBtn from "../../public/icons/minus.svg";
 
 const index = () => {
   return (
@@ -12,6 +15,14 @@ const index = () => {
       <div id="line-container" className="flex">
         <LineInfoList />
         <LineSearch />
+      </div>
+      <div id="map-btn" className="flex column">
+        <button type="button">
+          <Image src={PlusBtn} alt="확대" />
+        </button>
+        <button type="button">
+          <Image src={MinusBtn} alt="축소" />
+        </button>
       </div>
       <div id="select-container" className="flex justify-center">
         <LineSelectedBar />
