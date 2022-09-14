0dd5c23aec7c359c692ef89048b00335fc961b26
Yeom-JinHo <pwlsghq@naver.com>
2022-09-14 10:38:04 +0900

    feat : 카드 컴포넌트 추가

--- frontend/pages/subway/result.tsx
+++ frontend/pages/subway/result.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import LineLoadingBar from "../../components/subway/LineLoadingBar";
+import ResultCard from "../../components/theme/ResultCard";
 import styles from "./result.module.scss";
 
 const result = () => {
   return (
-    <div>
+    <div className={`${styles.root} flex align-center justify-center`}>
       <div className={`${styles.wrapper} flex`}>
         <LineLoadingBar color="LS" name="강남1" />
         <LineLoadingBar color="LS" name="강남" />
@@ -14,6 +15,12 @@ const result = () => {
         <LineLoadingBar color="LS" name="판교" />
       </div>
       <div className={styles.dimmer} />
+      <div className={`${styles.card} flex column align-center`}>
+        <ResultCard />
+        <button className="notoBold fs-24" type="button">
+          다시시작하기
+        </button>
+      </div>
     </div>
   );
 };
