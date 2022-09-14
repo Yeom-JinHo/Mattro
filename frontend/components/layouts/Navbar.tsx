c58f4a5636ff2e7e391ec458d9e3fb025ef8d0a9
jmlee9707 <jmlee9707@naver.com>
2022-09-14 13:47:17 +0900

    fix : navbar 메인에서는 색 다르게

--- frontend/components/layouts/Navbar.tsx
+++ frontend/components/layouts/Navbar.tsx
@@ -1,16 +1,26 @@
 import Link from "next/link";
 import Image from "next/image";
 // import { useRouter } from "next/router";
+import { useRouter } from "next/router";
 import styles from "./Navbar.module.scss";
 import before from "../../public/icons/before_menu.svg";
 // import DarkMode from "./DarkMode";
 
 export default function Navbar() {
+  const { pathname } = useRouter();
+
   const transition = () => {
+    // 클릭시에 햄버거 버튼 애니메이션 줄거에요
     console.log("aa");
   };
   return (
-    <header className={styles.navbar}>
+    <header
+      className={
+        pathname === "/"
+          ? `${styles.navbar_main} flex`
+          : `${styles.navbar} flex`
+      }
+    >
       <Link href="/">
         <div>LOGO</div>
       </Link>
