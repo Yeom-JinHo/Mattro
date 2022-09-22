f290e1583eab17f9796966e08f13fc0911d565e7
jmlee9707 <jmlee9707@naver.com>
2022-09-22 13:28:51 +0900

    feat : S07P22C206-149 메뉴바 애니메이션

--- frontend/components/layouts/Navbar.jsx
+++ frontend/components/layouts/Navbar.jsx
@@ -1,60 +1,50 @@
 import Link from "next/link";
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import logo from "../../public/images/logo/logo.png";
+import logo from "../../public/images/logo/logo_gray.png";
+import logoNav from "../../public/images/logo/logo_nav.png";
+import sound from "../../public/icons/volume_off.svg";
 // import { useRouter } from "next/router";
 import styles from "./Navbar.module.scss";
-import before from "../../public/icons/before_menu.svg";
 // import DarkMode from "./DarkMode";
 
 export default function Navbar() {
+  // console.log(document.querySelector("body"));
   const { pathname } = useRouter();
+  const router = useRouter();
   const menuRef = useRef();
   const itemsRef = useRef();
+  const bodyRef = useRef();
+  const [clicked, setClicked] = useState(false);
 
-  // let body;
-  // const body = document.querySelector("body");
-  // const init = function init() {
-  //   const body = document.querySelector("body");
-  // };
+  const toggleClass = (element, stringClass) => {
+    // 해당 요소의 클래스 속성값을 추가, 같은 캘래스 명 있는 경우 무시
+    if (element.current.classList.contains(stringClass)) {
+      element.current.classList.remove(stringClass);
+      setClicked(false);
+    } else {
+      element.current.classList.add(stringClass);
+      setClicked(true);
+    }
+  };
 
-  // console.log(body);
+  const clickBtn = () => {
+    return toggleClass(bodyRef, styles.nav_active); // "nav-active");
+  };
 
-  // const toggleClass = (element, stringClass) => {
-  //   // 해당 요소의 클래스 속성값을 추가, 같은 캘래스 명 있는 경우 무시
-  //   if (element.classList.contains(stringClass)) {
-  //     element.classList.remove(stringClass);
-  //     console.log("dd");
-  //   }
-  // };
-
-  // const clickBtn = () => {
-  //   console.log("a");
-  //   toggleClass(body, "nav_active");
-  // };
+  const movePage = (url) => {
+    router.push(url);
+    setTimeout(function () {
+      if (bodyRef.current.classList.contains(styles.nav_active)) {
+        bodyRef.current.classList.remove(styles.nav_active);
+        setClicked(false);
+      }
+    }, 1000);
+  };
 
   return (
-    <>
-      <div className={styles.nav}>
-        <div className={styles.nav__content}>
-          <ul className={styles.nav__list}>
-            <li className={`${styles.nav__list_item} ${styles.active_nav}`}>
-              <div className={styles.hover_target}>home</div>
-            </li>
-            <li className={styles.nav__list_item}>
-              <div className={styles.hover_target}>subway</div>
-            </li>
-            <li className={styles.nav__list_item}>
-              <div className={styles.hover_target}>theme</div>
-            </li>
-            <li className={styles.nav__list_item}>
-              <div className={styles.hover_target}>game</div>
-            </li>
-          </ul>
-        </div>
-      </div>
-
+    <span ref={bodyRef} className="flex align-center justify-center column">
       <nav
         className={
           pathname === "/"
@@ -64,13 +54,20 @@ export default function Navbar() {
       >
         <Link href="/">
           <div className={styles.logo}>
-            <Image src={logo} alt="logo" className={styles.img} />
+            {clicked && (
+              <Image src={logoNav} alt="logo" className={styles.img} />
+            )}
+            {!clicked && <Image src={logo} alt="logo" className={styles.img} />}
           </div>
         </Link>
-        <div className={styles.nav_wrap}>
-          <div
-            // ref={menuRef}
-            // onClick={clickBtn}
+        <div className={`${styles.nav_wrap} flex align-center justify-center`}>
+          {/* <button type="button">
+            <Image src={sound} alt="sound" />
+          </button> */}
+          <button
+            type="button"
+            ref={menuRef}
+            onClick={clickBtn}
             className={`${styles.menu_icon} ${styles.hover_target}`}
           >
             <span
@@ -80,9 +77,88 @@ export default function Navbar() {
             <span
               className={`${styles.menu_icon__line} ${styles.menu_icon__line_right}`}
             />
-          </div>
+          </button>
         </div>
       </nav>
-    </>
+
+      <div className={`${styles.nav} flex justify-center`}>
+        <div className={styles.nav__content}>
+          <ul className={styles.nav__list}>
+            <li
+              ref={itemsRef}
+              className={
+                pathname === "/"
+                  ? `${styles.nav__list_item} ${styles.active_nav}`
+                  : `${styles.nav__list_item}`
+              }
+            >
+              <button
+                type="button"
+                onClick={() => {
+                  movePage("/");
+                }}
+                className={`${styles.hover_target} fs-60 coreExtra`}
+              >
+                HOME
+              </button>
+            </li>
+            <li
+              ref={itemsRef}
+              className={
+                pathname === "/subway"
+                  ? `${styles.nav__list_item} ${styles.active_nav}`
+                  : styles.nav__list_item
+              }
+            >
+              <button
+                type="button"
+                onClick={() => {
+                  movePage("/subway");
+                }}
+                className={`${styles.hover_target} fs-60 coreExtra`}
+              >
+                SUBWAY
+              </button>
+            </li>
+            <li
+              ref={itemsRef}
+              className={
+                pathname === "/theme/main"
+                  ? `${styles.nav__list_item} ${styles.active_nav}`
+                  : styles.nav__list_item
+              }
+            >
+              <button
+                type="button"
+                onClick={() => {
+                  movePage("/theme/main");
+                }}
+                className={`${styles.hover_target} fs-60 coreExtra`}
+              >
+                THEME
+              </button>
+            </li>
+            <li
+              ref={itemsRef}
+              className={
+                pathname === "/game"
+                  ? `${styles.nav__list_item} ${styles.active_nav}`
+                  : styles.nav__list_item
+              }
+            >
+              <button
+                type="button"
+                onClick={() => {
+                  movePage("/game");
+                }}
+                className={`${styles.hover_target} fs-60 coreExtra`}
+              >
+                GAME
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </span>
   );
 }
