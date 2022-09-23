885fac6ffecdefc2e84aec34dbf68a625543522b
조성민 <chob5864@gmail.com>
2022-09-23 12:23:37 +0900

    build: 기본 api 스켈레톤 코드

---
+++ spark-master/src/main/java/com/spark/spark/KeywordReviewList.java
@@ -0,0 +1,26 @@
+package com.spark.spark;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+@Data
+public class KeywordReviewList {
+
+    @Id
+    private String Id;
+    private String string1;
+    private String string2;
+    private String string3;
+    private String string4;
+    private String string5;
+    private String string6;
+    private String string7;
+    private String string8;
+    private String string9;
+    private String string10;
+    private String string11;
+    private String string12;
+    private String string13;
+    private String string14;
+    private String string15;
+}
