5be1954b0a68c22fa599766d0cb58ce0985fcec5
Yeom-JinHo <pwlsghq@naver.com>
2022-09-29 09:43:40 +0900

    fix : All 버튼 액티브

--- frontend/pages/subway/index.tsx
+++ frontend/pages/subway/index.tsx
@@ -55,7 +55,7 @@ const Index = () => {
   const handleLineOpacity = (lineId: UsedLineIdType, opacity: 0.1 | 1) => {
     const lines = document.querySelectorAll<HTMLElement>(`.${lineId}`);
     lines.forEach((line) => {
-      if (line.tagName === "SPAN") {
+      if (line.tagName !== "LI" && line.tagName !== "DIV") {
         line.style.opacity = opacity.toString();
       }
     });
