73930553d656cd527f089d8f0c81d6f67c2699c4
jmlee9707 <jmlee9707@naver.com>
2022-10-01 17:28:08 +0900

    fix : axios try-catch

--- frontend/pages/apis/recommend.js
+++ frontend/pages/apis/recommend.js
@@ -2,20 +2,30 @@ import { API } from "./index";
 
 // 취향별 맛집 추천 받기
 export const themeRecommend = async (choices) => {
-  const res = await API.get(`/individual/recommendation/${choices}`);
-  if (res.status === 500) {
-    console.log("Error");
-  } else return res.data;
+  try {
+    const res = await API.get(`/individual/recommendation/${choices}`);
+    return res.data;
+  } catch (error) {
+    if (error.res.status !== 500) {
+      return res.data;
+    } else {
+      console.log("Error");
+    }
+  }
 };
 
 // 인덱스 기반 추천 받기
 export const indexRes = async (data) => {
-  console.log(data);
-  const res = await API.get(`/individual/recommendation/list/${data}`);
-  console.log("call");
-  if (res.status === 500) {
-    console.log("Error");
-  } else return res.data;
+  try {
+    const res = await API.get(`/individual/recommendation/list/${data}`);
+    return res.data;
+  } catch (error) {
+    if (error.res.status !== 500) {
+      return res.data;
+    } else {
+      console.log("Error");
+    }
+  }
 };
 
 export const ex = () => {};
